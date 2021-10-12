433eb9ad9af50cfc097c3a70864a994c4fab2cba
mateo <teodc4447@gmail.com>
2021-10-11 21:39:43 -0300

    Volviendo a pusher los cambios del primer get

--- api/src/controlers/controlers.js
+++ api/src/controlers/controlers.js
@@ -8,35 +8,45 @@ const {
 } = require("../db.js")
 
 
-router.get("/", async (req, res) => {
-    const country = (await axios(`https://restcountries.com/v3/all`)).data
+router.get("/", async (req, res, next) => {
+    try {
 
-    country?.forEach(async e => {
-        const {
-            name,
-            flags,
-            region,
-            area,
-            population,
-        } = e;
-
-        await Country.findOrCreate({
-            where: {
-                name: name.common,
-                imagen: flags[0],
-                continente: region,
-                subregion: e?.subregion ? e.subregion : "no tiene subregion",
-                poblacion: population,
-                area: area,
-                capital: e?.capital ? e.capital[0] : "No tiene capital"
-
-            }
-        }).catch(e => {
-            res.status(404).send(e)
+        const country = (await axios(`https://restcountries.com/v3/all`)).data
+    
+        country?.forEach(async e => {
+            const {
+                name,
+                flags,
+                region,
+                area,
+                population,
+            } = e;
+    
+            await Country?.findOrCreate({
+                where: {
+                    name: name.common,
+                    imagen: flags[0],
+                    continente: region,
+                    subregion: e?.subregion ? e.subregion : "no tiene subregion",
+                    poblacion: population,
+                    area: area,
+                    capital: e?.capital ? e.capital[0] : "No tiene capital"
+    
+                }
+                
+            })
+    
+            Country.findAll()
+            .then(dbCountries => {
+                res.status(200).send(dbCountries)
+            })
+            
+    
         })
+    }catch (err){
+        next(err);
+    }
 
-    })
-    res.status(200).json(country)
 })
 
 module.exports = router
