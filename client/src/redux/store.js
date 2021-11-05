1975ab63850ee6e83eda10040a433f22b5a131cd
mateo <teodc4447@gmail.com>
2021-11-04 21:39:51 -0300

    arreglando temas de compatibilidad

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,8 +5,7 @@ import thunkMiddleware from "redux-thunk";
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunkMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunkMiddleware)
   )
 );
 
